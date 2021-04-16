beda9a4f93a3a7020475fc6038bf42ff76644755
Miguel Dominguez <migueldominguez@uvicate.com>
2021-04-16 12:12:17 -0600

    -FileManagement fix.

--- lib/FileManagement/FileManagement.dart
+++ lib/FileManagement/FileManagement.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:http/http.dart';
 import 'package:path_provider/path_provider.dart';
-import 'Model/ObjFile.dart';
+import 'package:utilities/FileManagement/Model/FileCache.dart';
 import 'database/Entity/EFile.dart';
 import 'database/database.dart';
 
@@ -56,7 +56,7 @@ class FileManagement
 
 	/// This creates a cache for files so it doesn't need to keep downloading the same file on each visit.
 	///
-	/// The list of files must contain an [ObjFile] structure and an existing database
+	/// The list of files must contain an [FileCache] structure and an existing database
 	/// to work correctly, the way this works is as follow:
 	/// - Check each file's id exist in the database.
 	/// - If not, insert it's data in the DB and save the file into the app document directory,
@@ -98,7 +98,7 @@ class FileManagement
 
 	/// This creates a cache for files so it doesn't need to keep downloading the same file on each visit.
 	///
-	/// The file must contain an [ObjFile] structure and an existing database
+	/// The file must contain an [FileCache] structure and an existing database
 	/// to work correctly, the way this works is as follow:
 	/// - Check if the file's id exist in the database.
 	/// - If not, insert it's data in the DB and save the file into the app document directory,
@@ -109,24 +109,24 @@ class FileManagement
 	///
 	/// Very useful to speed up loading times.
 	/// Meant to be used with an API that keeps track of files.
-	static Future<ObjFile> getLocalCacheFile(ObjFile file, String updatedAt,
+	static Future<FileCache> getLocalCacheFile(FileCache file,
 			{String dataBaseName = 'app_database.db'}) async {
 		final db = await $FloorAppDatabase.databaseBuilder(dataBaseName).build();
 		final cacheDao = db.getCache;
-		final data = await cacheDao.findFileById(file.getFileId);
+		final data = await cacheDao.findFileById(file.getId);
 		if (data == null) {
-			final fileCache = EFile(file.getFileId, file.getFileName, updatedAt);
+			final fileCache = EFile(file.getId, file.getName, file.getUpdatedAt);
 			cacheDao.insertFile(fileCache);
-			var response = await get(Uri.parse(file.getFileUrl));
-			FileManagement.saveFile(file.getFileName, response.bodyBytes);
+			var response = await get(Uri.parse(file.getURL));
+			FileManagement.saveFile(file.getName, response.bodyBytes);
 		} else {
-			if (data.updatedAt != updatedAt) {
-				var response = await get(Uri.parse(file.getFileUrl));
+			if (data.updatedAt != file.getUpdatedAt) {
+				var response = await get(Uri.parse(file.getURL));
 				cacheDao.updateFileFromCache(data);
-				FileManagement.saveFile(file.getFileName, response.bodyBytes);
+				FileManagement.saveFile(file.getName, response.bodyBytes);
 			}
 		}
-		file.setFileUrl = await FileManagement.getFilePath(file.getFileName);
+		file.setURL = await FileManagement.getFilePath(file.getName);
 		return file;
 	}
 
